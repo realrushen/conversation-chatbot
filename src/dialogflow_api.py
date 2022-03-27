3232367e4b2a592f8575536c1079c13971454282
Karakchiev Aleksandr <realrushen@gmail.com>
2022-03-28 00:32:05 +0300

    dialogflow api calls functions moved in separate file

---
+++ src/dialogflow_api.py
@@ -0,0 +1,30 @@
+from environs import Env
+from google.cloud import dialogflow
+
+# Load environment variables
+env = Env()
+env.read_env()
+
+# Constants
+LANGUAGE_CODE = 'ru-RU'
+PROJECT_ID = env.str('PROJECT_ID')
+
+
+def detect_intent_text(session_id: object, text: object, project_id: str = PROJECT_ID,
+                       language_code: str = LANGUAGE_CODE) -> str:
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -2,19 +2,18 @@
 
 import logging
 
-from google.cloud import dialogflow
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Load environment variables from .env file
+from src.dialogflow_api import detect_intent_text
+
 env = Env()
 env.read_env()
 
 # Constants
-LANGUAGE_CODE = 'ru-RU'
 TELEGRAM_BOT_TOKEN = env.str('TELEGRAM_BOT_TOKEN')
-PROJECT_ID = env.str('PROJECT_ID')
 
 # Enable logging
 logging.basicConfig(
@@ -29,33 +28,12 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(text='Здравствуйте')
 
 
-
-
-def detect_intent_text(session_id: object, text: object, project_id: str = PROJECT_ID, language_code: str = LANGUAGE_CODE) -> str:
-    """Returns the result of detect intent with texts as inputs.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    return response.query_result.fulfillment_text
-
 def reply_customer(update: Update, context: CallbackContext) -> None:
     """Reply customer using DialogFlow model"""
     chat_id = update.message.chat_id
     text = update.message.text
     reply = detect_intent_text(session_id=chat_id, text=text)
     update.message.reply_text(reply)
-
 
 
 def main() -> None:
