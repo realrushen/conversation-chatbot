c31ac1f0f0dcbf934b8a6cb4c8aa3b764867d164
Karakchiev Aleksandr <realrushen@gmail.com>
2022-03-29 00:18:04 +0300

    added monitoring for telegram bot

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python3
-
 import logging
 
 from environs import Env
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+
+from src.dialogflow_api import get_reply
+from src.log_handlers import TelegramChatHandler
 
 # Load environment variables from .env file
-from src.dialogflow_api import get_reply
-
 env = Env()
 env.read_env()
 
 # Constants
 TELEGRAM_BOT_TOKEN = env.str('TELEGRAM_BOT_TOKEN')
+LOGS_BOT_TOKEN = env.str('LOGS_BOT_TOKEN')
+CHAT_ID_FOR_LOGS = env.str('CHAT_ID_FOR_LOGS')
 DEBUG = env.int('DEBUG')
 
-# Enable logging
-if DEBUG:
-    logging_level = logging.DEBUG
-else:
-    logging_level = logging.INFO
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging_level
-)
-
 logger = logging.getLogger(__name__)
-
-
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    update.message.reply_text(text='Здравствуйте')
 
 
 def reply_customer(update: Update, context: CallbackContext) -> None:
@@ -42,17 +29,33 @@ def reply_customer(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(reply.text)
 
 
+def log_error(update: Update, context: CallbackContext) -> None:
+    """Log exception while handling update"""
+    logger.exception(context.error)
+
+
 def main() -> None:
     """Start the bot."""
+    # Enable logging
+    if DEBUG:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging_level
+    )
+    telegram_handler = TelegramChatHandler(token=LOGS_BOT_TOKEN, chat_id=CHAT_ID_FOR_LOGS)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    telegram_handler.setFormatter(formatter)
+    logger.addHandler(telegram_handler)
+    # Bot initialisation
     updater = Updater(TELEGRAM_BOT_TOKEN)
-
     dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_customer))
-
+    dispatcher.add_error_handler(log_error)
+    logger.info('Telegram support bot started')
     updater.start_polling()
-
     updater.idle()
 
 
